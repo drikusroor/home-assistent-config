76e151bd07daecf7fa1446ea6ae3a5694cc4a87b
Drikus Roor <drikusroor@gmail.com>
2020-05-19 08:58:35 +0200

    Add functioning cuckoo clock thanks to python

---
+++ python_scripts/cuckoo_clock.py
@@ -0,0 +1,27 @@
+# import time
+
+# play cuckoo clock an x amount of times
+entity_id = data.get("entity_id")
+hour = int(data.get("hour"))
+minute = int(data.get("minute"))
+
+amount = hour
+
+if hour > 12:
+    amount = hour - 12
+elif minute < 55 and minute > 5:
+    amount = 1
+
+if minute > 55:
+    amount += 1
+
+if entity_id is not None:
+    service_data = {
+        "entity_id": entity_id,
+        "media_content_id": "http://projects.kokokoding.nl/home-assistant/cuckoo.wav",
+        "media_content_type": "audio/wav",
+    }
+
+    for i in range(amount):
+        hass.services.call("media_player", "play_media", service_data, True)
+        time.sleep(1)
