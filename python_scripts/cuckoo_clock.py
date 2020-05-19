1905ff81992b0250a0fddad0567f60abb6493808
Drikus Roor <drikusroor@gmail.com>
2020-05-19 21:31:47 +0200

    Add time to work & fix cuckoo clock 30 minutes bug

--- python_scripts/cuckoo_clock.py
+++ python_scripts/cuckoo_clock.py
@@ -7,19 +7,18 @@ minute = int(data.get("minute"))
 
 amount = hour
 
-# Only allowed after work hours, otherwise the girlfriend gets angry
-if hour < 18:
-    return
 
 if hour > 12:
     amount = hour - 12
-elif minute < 55 and minute > 5:
-    amount = 1
 
 if minute > 55:
     amount += 1
 
-if entity_id is not None:
+if minute < 55 and minute > 5:
+    amount = 1
+
+# Only allowed after work hours, otherwise the girlfriend gets angry
+if entity_id is not None and hour >= 18:
     service_data = {
         "entity_id": entity_id,
         "media_content_id": "http://projects.kokokoding.nl/home-assistant/cuckoo.wav",
@@ -28,4 +27,4 @@ if entity_id is not None:
 
     for i in range(amount):
         hass.services.call("media_player", "play_media", service_data, True)
-        time.sleep(1)
+        time.sleep(1.5)
